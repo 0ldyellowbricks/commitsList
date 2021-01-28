c40950105f660256935432d9f2c2331c6ee0edcf
oldYellowBricks <fraych@gmail.com>
2021-01-28 13:24:16 -0800

    add table setting in CommitController

--- CommitList/CommitList/ViewController/CommitController.swift
+++ CommitList/CommitList/ViewController/CommitController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class CommitController: UITableViewController {
+    let cellId = "cellId"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,7 +17,16 @@ class CommitController: UITableViewController {
         setNavBar()
     }
     
-    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 5
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! CommitCell
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
     
     fileprivate func setNavBar() {
         navigationItem.title = "CommitsList"
@@ -26,7 +36,10 @@ class CommitController: UITableViewController {
         navigationController?.navigationBar.barTintColor = UIColor.rgb(r: 50, g: 199, b: 242)
         navigationController?.navigationBar.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
     }
-
+    fileprivate func setList() {
+        tableView.register(CommitCell.self, forCellReuseIdentifier: cellId)
+        tableView.tableFooterView = UIView()
+    }
 }
 
 
