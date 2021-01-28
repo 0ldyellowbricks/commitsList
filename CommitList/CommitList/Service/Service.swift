a0acc3f0f2ee06577794e6b6717c26133df8e756
oldYellowBricks <fraych@gmail.com>
2021-01-28 14:26:53 -0800

    add URLSession code

--- CommitList/CommitList/Service/Service.swift
+++ CommitList/CommitList/Service/Service.swift
@@ -7,4 +7,38 @@
 
 import Foundation
 
-
+class Service {
+    static let shared = Service()
+    
+    func getResults(completion: @escaping (Result<[CommitResult], ErrorMessage>) -> ()) {
+        
+        let urlString = "https://api.github.com/repos/0ldyellowbricks/commitsList/commits"
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let _ = error {
+                completion(.failure(.invalidData))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.invalidData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let results = try decoder.decode([CommitResult].self, from: data)
+                completion(.success(results))
+            } catch {
+                completion(.failure(.invalidData))
+            }
+        }.resume()
+    }
+}
