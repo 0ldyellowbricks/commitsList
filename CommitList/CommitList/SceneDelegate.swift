6c0c95c4ac866aaf0745a0bcb46f0e486d7b92fb
oldYellowBricks <fraych@gmail.com>
2021-01-28 13:17:02 -0800

    add navigationbar setting

--- CommitList/CommitList/SceneDelegate.swift
+++ CommitList/CommitList/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = UINavigationController(rootViewController: CommitController())
+        window?.rootViewController = CustomNavigationCotroller(rootViewController: CommitController())
         window?.makeKeyAndVisible()
     }
 
--- CommitList/CommitList/ViewController/CommitController.swift
+++ CommitList/CommitList/ViewController/CommitController.swift
@@ -13,6 +13,28 @@ class CommitController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view. 
         self.view.backgroundColor = .red
+        setNavBar()
+    }
+    
+    
+    
+    fileprivate func setNavBar() {
+        navigationItem.title = "CommitsList"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.backgroundColor = .blue
+        navigationController?.navigationBar.barTintColor = .green
+        navigationController?.navigationBar.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
     }
 
+}
+
+
+
+
+
+class CustomNavigationCotroller: UINavigationController {
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
 }
