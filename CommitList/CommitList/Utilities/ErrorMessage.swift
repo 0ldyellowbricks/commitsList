7a41bc9227e317af4f824a10fd7663bc9401de1d
oldYellowBricks <fraych@gmail.com>
2021-01-28 14:16:36 -0800

    add error message file

---
+++ CommitList/CommitList/Utilities/ErrorMessage.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorMessage.swift
+//  CommitList
+//
+//  Created by Jing Zhao on 1/28/21.
+//
+
+import Foundation
+
+enum ErrorMessage: String, Error {
+    case invalidData = "Sorry. Somthing went wrong"
+    case invalidResponse = "Server error"
+}
