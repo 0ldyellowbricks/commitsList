f7f92d1c8c1ee8856f2e845e27c6531a1976dd24
oldYellowBricks <fraych@gmail.com>
2021-01-28 13:45:14 -0800

    add model file

---
+++ CommitList/CommitList/Model/CommitResult.swift
@@ -0,0 +1,23 @@
+//
+//  CommitResult.swift
+//  CommitList
+//
+//  Created by Jing Zhao on 1/28/21.
+//
+
+import Foundation
+
+struct CommitResult: Codable {
+    var sha: String?
+    var commit: Commit?
+}
+
+struct Commit: Codable {
+    var author: Author?
+    var message: String
+}
+
+struct Author: Codable {
+    var name: String?
+    var date: String?
+}
