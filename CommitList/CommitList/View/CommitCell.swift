d257ea5d9b12f1cde9282d326e506f469645322c
Jing Zhao <oldyellowbricks@Jings-MacBook-Pro.local>
2021-12-27 13:02:20 -0800

    practice git command on commitcell file

--- CommitList/CommitList/View/CommitCell.swift
+++ CommitList/CommitList/View/CommitCell.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class CommitCell: UITableViewCell {
+//    test for commit this file 
     var cellCommitVM: CommitViewModel! {
         didSet {
             msgLabel.text = cellCommitVM.message
@@ -19,7 +20,6 @@ class CommitCell: UITableViewCell {
         let lbl = UILabel(frame: CGRect(x: 10, y: 10, width: kScreenWitdh - 20, height: 60))
         lbl.numberOfLines = 0
         lbl.font = .boldSystemFont(ofSize: 16)
-//        lbl.backgroundColor = .blue
         lbl.text = "1111111"
         return lbl
     }()
@@ -27,14 +27,12 @@ class CommitCell: UITableViewCell {
         let lbl = UILabel(frame: CGRect(x: 10, y: 80, width: kScreenWitdh - 20, height: 25))
         lbl.textColor = UIColor.rgb(r: 50, g: 199, b: 242)
         lbl.font = .systemFont(ofSize: 14)
-//        lbl.backgroundColor = .yellow
         lbl.text = "1111111"
         return lbl
     }()
     let authorDateLabel: UILabel = {
         let lbl = UILabel(frame: CGRect(x: 10, y: 105, width: kScreenWitdh - 20, height: 25))
         lbl.font = .systemFont(ofSize: 14)
-//        lbl.backgroundColor = .orange
         lbl.text = "1111111"
         return lbl
     }()
