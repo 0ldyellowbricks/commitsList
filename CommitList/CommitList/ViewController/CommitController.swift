def1ae0d3470607c3348f7fbde71d3621202f35c
oldYellowBricks <oldyellowbricks@oldYellowBrickss-MacBook-Pro.local>
2022-03-11 09:18:56 -0800

    master added 2 lines

--- CommitList/CommitList/ViewController/CommitController.swift
+++ CommitList/CommitList/ViewController/CommitController.swift
@@ -96,3 +96,5 @@ class CustomNavigationCotroller: UINavigationController {
 }
 // master edite mar 09 2022 --1
 // master edite mar 09 2022 --2
+// master edite mar 11 2022 --3
+// master edite mar 11 2022 --4
